f5f9596e03fd3fadbf1b212ad5f30380e2fa7cb3
JohnCodes <93669966+ken0gul@users.noreply.github.com>
2023-12-14 17:22:46 -0500

    styled register component and added new font

--- client-wise-ui/src/pages/Register.jsx
+++ client-wise-ui/src/pages/Register.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 const Register = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
   const submitUserLogin = async (e) => {
     e.preventDefault();
     const { data } = await axios.post("http://localhost:3001/register", {
@@ -14,30 +15,52 @@ const Register = () => {
     setUsername("");
     setPassword("");
   };
+
   return (
-    <div>
-      <form onSubmit={submitUserLogin}>
-        <div>
-          <label htmlFor="username">Username</label>
+    <div className="min-h-screen flex items-center justify-center">
+      <form
+        onSubmit={submitUserLogin}
+        className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full"
+      >
+        <h2 className="text-2xl font-semibold mb-6 text-gray-800">
+          Create an Account
+        </h2>
+        <div className="mb-4">
           <input
             type="text"
             id="username"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            className="border-2 border-gray-400"
+            className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:ring focus:border-indigo-300"
+            placeholder="Username"
           />
         </div>
-        <div>
-          <label htmlFor="password">Password</label>
+        <div className="mb-4">
           <input
             type="password"
             id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="border-2 border-gray-400"
+            className="mt-1 p-2 w-full border rounded-md focus:outline-none focus:ring focus:border-indigo-300"
+            placeholder="Password"
           />
         </div>
-        <button className="border-2 px-4 py-2 rounded m-2">Register</button>
+        <button
+          type="submit"
+          className="bg-indigo-500 text-white px-4 py-2 rounded hover:bg-indigo-600 focus:outline-none focus:ring focus:border-indigo-300"
+        >
+          Register
+        </button>
+        <p className="mt-4 text-gray-600 text-center">
+          Already a member?{" "}
+          <button
+            type="button"
+            className="text-indigo-500 underline hover:text-indigo-600 focus:outline-none"
+            onClick={() => alert("Redirect to login page")}
+          >
+            Login
+          </button>
+        </p>
       </form>
     </div>
   );
